f85be57df94dbab8af62aa855afa5aa7df257765
Zenon <duo.1080@gmail.com>
2024-09-07 02:09:25 +0800

    config community

--- lua/community.lua
+++ lua/community.lua
@@ -8,20 +8,24 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- example of importing an entire language pack
-  -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
+  { import = "astrocommunity.pack.zig" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.vue" },
-  -- { import = "astrocommunity.recipes.heirline-clock-statusline" },
-  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
-  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   { import = "astrocommunity.search.nvim-hlslens" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.media.codesnap-nvim" },
+  {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    opts = {
+      save_path = "~/Pictures/Screenshots/",
+      code_font_family = "JetBrainsMono Nerd Font",
+      bg_color = "#a8b0d5",
+    },
+  },
 }
