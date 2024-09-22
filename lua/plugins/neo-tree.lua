0d43d7b96d73b18b40819c8f78076fadbf5f94ce
Zenon <duo.1080@gmail.com>
2024-09-22 23:10:23 +0800

    config neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,15 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  init = function()
+    -- 自定义 Git 状态的高亮颜色
+    vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#117554", bg = "NONE", bold = true }) -- 绿色
+    vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#ff9900", bg = "NONE", bold = true }) -- 橙色
+    vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#A04747", bg = "NONE", bold = true }) -- 红色
+    vim.api.nvim_set_hl(0, "NeoTreeGitRenamed", { fg = "#ffa500", bg = "NONE", bold = true }) -- 橙色
+    vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#117554", bg = "NONE", bold = true }) -- 青色
+    vim.api.nvim_set_hl(0, "NeoTreeGitIgnored", { fg = "#808080", bg = "NONE", bold = true }) -- 灰色
+  end,
+
   opts = {
     enable_diagnostics = true,
     default_component_configs = {
@@ -9,7 +19,7 @@ return {
           modified = "󰬔",
           deleted = "󰬋",
           renamed = "󰬙",
-          untracked = "󰎔",
+          untracked = "󰬟",
           ignored = "󰬐",
           unstaged = "󰬜",
           staged = "󰬚",
@@ -29,6 +39,37 @@ return {
           -- 'thumbs.db',
         },
         never_show = {},
+      },
+      git = {
+        enable = true,
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true,
+          },
+        },
+        directory = {
+          {
+            "icon",
+            default = true,
+          },
+          {
+            "name",
+            zindex = 1,
+          },
+          {
+            "git_status", -- 添加 git 状态显示
+            zindex = 0,
+          },
+        },
+        file = {
+          { "icon" },
+          { "name", use_git_status_colors = true },
+          { "diagnostics" },
+          { "git_status", highlight = "NeoTreeDimText" },
+        },
       },
     },
   },
